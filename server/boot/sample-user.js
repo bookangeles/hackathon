79475396a34d75eb49c4b5e3f1a70d05215e40e4
Boris Chumichev <boris362@yandex.ru>
2016-02-20 21:29:29 +0100

    Share model

--- server/boot/sample-user.js
+++ server/boot/sample-user.js
@@ -1,8 +1,16 @@
 module.exports = function(app) {
   var Client = app.models.Client;
+  var Share = app.models.Share;
 
   Client.create([
     {displayName: 'John', email: 'j@j.com', password: 'sesame'}
+  ], function (err, res) {
+    if (err) return console.log('fail')
+    console.log('user created', res)
+  })
+
+  Share.create([
+    {owner: 'John', email: 'j@j.com', password: 'sesame'}
   ], function (err, res) {
     if (err) return console.log('fail')
     console.log('user created', res)
