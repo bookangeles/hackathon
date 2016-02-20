13dae1c77cc69b9a8d1fedd79f62eb2cb8b47d40
Boris Chumichev <boris362@yandex.ru>
2016-02-20 13:20:03 +0100

    jade template engine

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,5 @@
 module.exports = function(server) {
-  // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/', (req, res) => res.render('index'));
   server.use(router);
 };
