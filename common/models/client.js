df494dbdd2145b9aa4d691d8ed3d3764b62541ad
Boris Chumichev <boris362@yandex.ru>
2016-02-20 19:59:27 +0100

    Email verification flow

--- common/models/client.js
+++ common/models/client.js
@@ -1,6 +1,23 @@
+var path = require('path');
+
 module.exports = function(Client) {
   Client.beforeRemote('create', function(context, unused, next) {
-    console.log(context.req.body)
+    
     next();
+  });
+   
+  Client.afterRemote('create', function(context, user, next) {
+    user.verify({
+      type: 'email',
+      to: user.email,
+      from: 'bookangeles.com@gmail.com',
+      subject: 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+      redirect: '/verified',
+      user: user
+    }, function(err, response, next) {
+      if (err) return next(err);
+      context.res.render('index');
+    });
   });
 };
