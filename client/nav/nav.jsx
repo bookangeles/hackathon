7d5f4a1c542d88217bd43f6107d99b0e6cf32718
Ivan Kotov <ivn@dreamindustries.co>
2016-02-21 12:50:13 +0300

    global CSS

--- client/nav/nav.jsx
+++ client/nav/nav.jsx
@@ -5,15 +5,17 @@ import { Link } from 'react-router';
 export default React.createClass({
   render() {
     return (
-      <nav>
-        <ul role="nav">
-          <li><Link to="/">Library</Link></li>
-          <li><Link to="/storage">Storage</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/form">Upload a book</Link></li>
-        </ul>
+      <div className="page">
+        <nav>
+          <ul role="nav">
+            <li><Link to="/">Library</Link></li>
+            <li><Link to="/storage">Storage</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/form">Upload a book</Link></li>
+          </ul>
+        </nav>
         {this.props.children}
-      </nav>
+      </div>
     );
   },
 });
