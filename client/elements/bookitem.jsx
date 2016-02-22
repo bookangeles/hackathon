468c95ac466c8e487e4ce25de3ad764bb5d36c15
Ivan Kotov <ivn@dreamindustries.co>
2016-02-22 03:05:18 +0300

    book cards

--- client/elements/bookitem.jsx
+++ client/elements/bookitem.jsx
@@ -1,5 +1,7 @@
 require('./bookitem.styl');
 import Cloud from '../elements/cloud.jsx';
+import Paper from 'material-ui/lib/paper';
+import OpenIcon from 'material-ui/lib/svg-icons/action/open-in-new';
 
 const BookItem = React.createClass({
   mixins: [Bem],
@@ -13,16 +15,23 @@ const BookItem = React.createClass({
   },
 
   render() {
+    const token = _.get(document.cookie.match(/auth_token=(\w+)/), 1);
     const book = this.props;
-    const tmp = this.props.title.split(' ');
-    const caption = _.first(tmp) + '...' + _.last(tmp);
+    const tmp = this.props.fileName.split(' ');
+    const caption = tmp.length > 2
+      ? _.first(tmp) + '...' + _.last(tmp)
+      : this.props.fileName;
 
     return (
-      <div className={this.b_()}>
-        <a href={`book/${book.id}`} className={this.b_('-caption')} title={book.title}>{caption}</a>
-        <span className={this.b_('-author')} title={book.author}>{book.author}</span>
+      <Paper className={this.b_()} zDepth={2}>
+        <a href={`book/${book.id}`} className={this.b_('-caption')} title={book.fileName}>{caption}</a>
+        <a href={`/bapi/books/${book.id}?access_token=${token}`} target="_blank">
+          <OpenIcon className={this.b_('-plink')} />
+        </a>
+        <div className={this.b_('-author')} title={book.author}>{book.author}</div>
+
         <Cloud tags={book.tags} modifier="inCard" />
-      </div>
+      </Paper>
     );
   },
 });
